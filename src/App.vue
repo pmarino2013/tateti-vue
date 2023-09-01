264c73759f8b4e0ddaeab0cf727b20c4ce31580b
Pablo Marino <pmarino2013@gmail.com>
2023-08-31 22:48:54 -0300

    Version 1.0.0 con juego funcionando

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import TatetiApp from "./components/TatetiApp.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-8 offset-md-2">
+        <TatetiApp />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/TatetiApp.vue
@@ -0,0 +1,203 @@
+<script setup>
+import { onMounted, ref, watch } from "vue";
+
+const posiciones = ref([]);
+const winner = ref(false);
+const empate = ref(false);
+const loser = ref(false);
+
+onMounted(() => {
+  InicializarJuego();
+});
+watch(winner, (newWinner) => {
+  if (newWinner) {
+    setTimeout(() => {
+      alert("Ganaste!!");
+      InicializarJuego();
+      winner.value = false;
+    }, 500);
+  }
+});
+
+watch(empate, (newEmpate) => {
+  if (newEmpate) {
+    setTimeout(() => {
+      alert("Empate");
+      InicializarJuego();
+      empate.value = false;
+    }, 500);
+  }
+});
+
+watch(loser, (newLoser) => {
+  if (newLoser) {
+    setTimeout(() => {
+      alert("Perdiste :(");
+      InicializarJuego();
+      loser.value = false;
+    }, 500);
+  }
+});
+
+const InicializarJuego = () => {
+  posiciones.value = [
+    { posicion: 1, done: "", jugador: "", completo: false },
+    { posicion: 2, done: "", jugador: "", completo: false },
+    { posicion: 3, done: "", jugador: "", completo: false },
+    { posicion: 4, done: "", jugador: "", completo: false },
+    { posicion: 5, done: "", jugador: "", completo: false },
+    { posicion: 6, done: "", jugador: "", completo: false },
+    { posicion: 7, done: "", jugador: "", completo: false },
+    { posicion: 8, done: "", jugador: "", completo: false },
+    { posicion: 9, done: "", jugador: "", completo: false },
+  ];
+};
+
+const marcarCasillero = (id) => {
+  if (!winner.value) {
+    posiciones.value[id].done = "X";
+    posiciones.value[id].jugador = "player";
+    posiciones.value[id].completo = true;
+
+    ganar();
+  }
+};
+
+const ganar = () => {
+  switch (true) {
+    case posiciones.value[0].done == "X" &&
+      posiciones.value[1].done == "X" &&
+      posiciones.value[2].done == "X":
+      winner.value = true;
+      break;
+    case posiciones.value[3].done == "X" &&
+      posiciones.value[4].done == "X" &&
+      posiciones.value[5].done == "X":
+      winner.value = true;
+      break;
+    case posiciones.value[6].done == "X" &&
+      posiciones.value[7].done == "X" &&
+      posiciones.value[8].done == "X":
+      winner.value = true;
+      break;
+    case posiciones.value[0].done == "X" &&
+      posiciones.value[3].done == "X" &&
+      posiciones.value[6].done == "X":
+      winner.value = true;
+      break;
+    case posiciones.value[1].done == "X" &&
+      posiciones.value[4].done == "X" &&
+      posiciones.value[7].done == "X":
+      winner.value = true;
+      break;
+    case posiciones.value[2].done == "X" &&
+      posiciones.value[5].done == "X" &&
+      posiciones.value[8].done == "X":
+      winner.value = true;
+      break;
+    case posiciones.value[0].done == "X" &&
+      posiciones.value[4].done == "X" &&
+      posiciones.value[8].done == "X":
+      winner.value = true;
+      break;
+    case posiciones.value[2].done == "X" &&
+      posiciones.value[4].done == "X" &&
+      posiciones.value[6].done == "X":
+      winner.value = true;
+      break;
+    default:
+      bot();
+      break;
+  }
+};
+
+const perder = () => {
+  switch (true) {
+    case posiciones.value[0].done == "O" &&
+      posiciones.value[1].done == "O" &&
+      posiciones.value[2].done == "O":
+      loser.value = true;
+      break;
+    case posiciones.value[3].done == "O" &&
+      posiciones.value[4].done == "O" &&
+      posiciones.value[5].done == "O":
+      loser.value = true;
+      break;
+    case posiciones.value[6].done == "O" &&
+      posiciones.value[7].done == "O" &&
+      posiciones.value[8].done == "O":
+      loser.value = true;
+      break;
+    case posiciones.value[0].done == "O" &&
+      posiciones.value[3].done == "O" &&
+      posiciones.value[6].done == "O":
+      loser.value = true;
+      break;
+    case posiciones.value[1].done == "O" &&
+      posiciones.value[4].done == "O" &&
+      posiciones.value[7].done == "O":
+      loser.value = true;
+      break;
+    case posiciones.value[2].done == "O" &&
+      posiciones.value[5].done == "O" &&
+      posiciones.value[8].done == "O":
+      loser.value = true;
+      break;
+    case posiciones.value[0].done == "O" &&
+      posiciones.value[4].done == "O" &&
+      posiciones.value[8].done == "O":
+      loser.value = true;
+      break;
+    case posiciones.value[2].done == "O" &&
+      posiciones.value[4].done == "O" &&
+      posiciones.value[6].done == "O":
+      loser.value = true;
+      break;
+  }
+};
+
+const bot = () => {
+  let disponibles = posiciones.value.filter((pos) => pos.completo == false);
+  if (disponibles.length > 0) {
+    let numeroRandom = Math.floor(Math.random() * 9);
+
+    if (!posiciones.value[numeroRandom].completo) {
+      posiciones.value[numeroRandom].done = "O";
+      posiciones.value[numeroRandom].jugador = "bot";
+      posiciones.value[numeroRandom].completo = true;
+      perder();
+    } else {
+      bot();
+    }
+  } else {
+    empate.value = true;
+  }
+};
+</script>
+<template>
+  <div class="container">
+    <div class="row mt-5">
+      <div v-for="(posicion, index) in posiciones" class="col-4">
+        <div
+          class="card d-flex justify-content-center align-items-center my-2"
+          @click="marcarCasillero(index)"
+        >
+          <div v-if="posicion.done == 'X' && posicion.jugador == 'player'">
+            <span class="simbolo">X</span>
+          </div>
+          <div v-else-if="posicion.done == 'O' && posicion.jugador == 'bot'">
+            <span class="simbolo">O</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scope>
+.card {
+  height: 200px;
+}
+.simbolo {
+  font-size: 100px;
+}
+</style>
